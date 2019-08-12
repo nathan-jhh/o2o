a61634f1802a1380ba1bdeaff83f68c9600cb50f
nathan-jhh <jhaoheng@foxmail.com>
2019-08-12 15:51:17 +0800

    完成前后端联调

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
@@ -1,10 +1,9 @@
 package com.imooc.o2o.web.shopadmin;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,14 +19,17 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imooc.o2o.dto.ShopExecution;
+import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.entity.PersonInfo;
 import com.imooc.o2o.entity.Shop;
+import com.imooc.o2o.entity.ShopCategory;
 import com.imooc.o2o.enums.ShopStateEnum;
 import com.imooc.o2o.exceptions.ShopOperationException;
+import com.imooc.o2o.service.AreaService;
+import com.imooc.o2o.service.ShopCategoryService;
 import com.imooc.o2o.service.ShopService;
+import com.imooc.o2o.util.CodeUtil;
 import com.imooc.o2o.util.HttpServletRequestUtil;
-import com.imooc.o2o.util.ImageUtil;
-import com.imooc.o2o.util.PathUtil;
 
 // 负责店铺管理逻辑
 @Controller
@@ -35,10 +37,42 @@ import com.imooc.o2o.util.PathUtil;
 public class ShopManagementController {
 	@Autowired
 	private ShopService shopService;
+	@Autowired
+	private ShopCategoryService shopCategoryService;
+	@Autowired
+	private AreaService areaService;
+	
+	@RequestMapping(value = "/getshopinitinfo",method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> getShopInitInfo(){
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		List<ShopCategory> shopCategoryList = new ArrayList<ShopCategory>();
+		List<Area> areaList = new ArrayList<Area>();
+		try {
+			shopCategoryList = shopCategoryService.getShopCategoryList(new ShopCategory());
+			areaList = areaService.getAreaList();
+			modelMap.put("shopCategoryList", shopCategoryList);
+			modelMap.put("areaList", areaList);
+			modelMap.put("success", true);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.getMessage());
+			
+		}
+		return modelMap; // 变为json 传到前台
+		
+	}
+	
 	@RequestMapping(value = "/registershop", method = RequestMethod.POST)
 	@ResponseBody // 自动转换为json对象
 	private Map<String, Object> registerShop(HttpServletRequest request){
 		Map<String, Object> modelMap = new HashMap<String, Object>(); 
+		if(!CodeUtil.checkVerifyCode(request)) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "输入了错误的验证码");
+			return modelMap;
+		}
+		
 		// 1.接收并转化相应参数，包括店铺信息以及图片信息
 		String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
 		ObjectMapper mapper = new ObjectMapper();	//json转换成shop实体类
--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -5,14 +5,11 @@ $(function () {
    // 获取初始url
    var initUrl = '/o2o/shopadmin/getshopinitinfo';
    var registerShopUrl = '/o2o/shopadmin/registershop';
-   alert(initUrl);
    getShopInitInfo();
-
 
    function getShopInitInfo(){
       // 获取初始值
       $.getJSON(initUrl, function (data) {
-          alert("SSSSSSSS");
          if (data.success) {
             var tempHtml = '';
             var tempAreaHtml = '';
@@ -31,51 +28,59 @@ $(function () {
       });
    }
       // 提交
-      $("#submit").click(function () {
-         var shop = {};
-         shop.shopName = $("#shop-name").val();
-         shop.shopAddr = $("#shop-addr").val();
-         shop.phone = $("#phone").val();
-         shop.shopDesc = $("#shop-desc").val();
-         shop.shopCategory = {
-            shopCategoryId:$("#shop-category").find("option").not(function () {
-                return !this.selected;
-            }).data("id")
-         };
-         shop.area = {
-            areaId: $("#area").find("option").not(function () {
-               return !this.selected;
-            }).data("id")
-         };
-         var shopImg = $("#shop-img")[0].file[0];
-         var formData = new FormData();
-         formData.append("shopImg", shopImg);
-         formData.append("shopStr", JSON.stringify(shop));
-         var verifyCodeActual = $("#j_captcha").val();
-         // 判断是否为空
-         if (!verifyCodeActual) {
-             $.toast("请输入验证码!");
-             return;
-         }
-         formData.append("verifyCodeActual", verifyCodeActual);
-         // 提交到后台
-          $.ajax({
-              url: registerShopUrl,
-              type: "POST",
-              data: formData,
-              contentType: false,
-              processDate: false,
-              cache: false,
-              success: function (data) {
-                 if (data.success){
-                     $.toast("提交成功!");
-                 } else {
-                     $.toast("提交失败" + data.errMsg);
-                 }
-                 // 不管没有提交成功 都更换验证码
-                 $("#captcha_img").click();
-              }
-
-          });
-      });
+   $('#submit').click(function() {
+	    // 创建shop对象
+	    var shop = {};
+	 
+	    // 获取表单里的数据并填充进对应的店铺属性中
+	    shop.shopName = $('#shop-name').val();
+	    shop.shopAddr = $('#shop-addr').val();
+	    shop.phone = $('#shop-phone').val();
+	    shop.shopDesc = $('#shop-desc').val();
+	    // 选择选定好的店铺类别
+	    shop.shopCategory = {
+	        shopCategoryId : $('#shop-category').find('option').not(function() {
+	            return !this.selected;
+	        }).data('id')
+	    };
+	    // 选择选定好的区域信息
+	    shop.area = {
+	        areaId : $('#area').find('option').not(function() {
+	            return !this.selected;
+	        }).data('id')
+	    };
+	    // 获取上传的图片文件流
+	    var shopImg = $('#shop-img')[0].files[0];
+	    // 生成表单对象，用于接收参数并传递给后台
+	    var formData = new FormData();
+	    // 添加图片流进表单对象里
+	    formData.append('shopImg', shopImg);
+	    // 将shop json对象转成字符流保存至表单对象key为shopStr的的键值对里
+	    formData.append('shopStr', JSON.stringify(shop));
+	    // 获取表单里输入的验证码
+	    var verifyCodeActual = $('#j_captcha').val();
+	    if (!verifyCodeActual) {
+	        $.toast('请输入验证码！');
+	        return;
+	    }
+	    formData.append('verifyCodeActual', verifyCodeActual);
+	    // 将数据提交至后台处理相关操作
+	    $.ajax({
+	        url : registerShopUrl,
+	        type : 'POST',
+	        data : formData,
+	        contentType : false,
+	        processData : false,
+	        cache : false,
+	        success : function(data) {
+	            if (data.success) {
+	                $.toast('提交成功！');
+	            } else {
+	                $.toast('提交失败！' + data.errMsg);
+	            }
+	            // 点击验证码图片的时候，注册码会改变
+	            $('#captcha_img').click();
+	        }
+	    });
+	});
 });
