2b071ff23c9e5ef47c462414c4ad87df96f46514
nathan-jhh <jhaoheng@foxmail.com>
2019-08-20 23:38:24 +0800

    引入redis缓存

--- src/test/java/com/imooc/o2o/BaseTest.java
+++ src/test/java/com/imooc/o2o/BaseTest.java
@@ -7,7 +7,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //初始化spring配置,配置spring和junit整合，junit启动时加载springIOC容器
 @RunWith(SpringJUnit4ClassRunner.class)
 //告诉junit spring配置文件的位置
-@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml",
+						"classpath:spring/spring-redis.xml"})
 public class BaseTest {
 	
 }
--- src/test/java/com/imooc/o2o/service/AreaServiceTest.java
+++ src/test/java/com/imooc/o2o/service/AreaServiceTest.java
@@ -13,10 +13,14 @@ import com.imooc.o2o.entity.Area;
 public class AreaServiceTest extends BaseTest{
 	@Autowired
 	private AreaService areaService;
+	@Autowired
+	private CacheService cacheService;
 	@Test
 	public void testGetAreaList() {
 		List<Area> areaList = areaService.getAreaList();
 		System.out.println(areaList.size());
-		//assertEquals("西苑", areaList.get(0).getAreaName());
+		assertEquals("南苑", areaList.get(0).getAreaName());
+		cacheService.removeFromCache(areaService.AREALISTKEY);
+		areaList = areaService.getAreaList();
 	}
 }
